6a9c3ee03dc982364344eda97b468c7616778258
bearkerb <gabrieltan7879@gmail.com>
2025-08-20 12:37:13 +0800

    Added echoing of user inputs, and exit upon bye command

--- src/main/java/Lucid.java
+++ src/main/java/Lucid.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Lucid {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Lucid.\nWhat can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("Hello! I'm Lucid.\nHow can I help you?");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = "";
+        while (true) {
+            userInput = scanner.nextLine();
+            if (userInput.equals("bye")) {
+                System.out.println("Goodbye. Until next time!");
+                return;
+            }
+            System.out.println(userInput);
+        }
     }
 }
