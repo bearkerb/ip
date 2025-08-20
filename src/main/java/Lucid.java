1f1fe745e475440296685eb25d9a83269b51c2cf
bearkerb <gabrieltan7879@gmail.com>
2025-08-20 12:22:22 +0800

    Renaming, greet and exit

---
+++ src/main/java/Lucid.java
@@ -0,0 +1,6 @@
+public class Lucid {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Lucid.\nWhat can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
