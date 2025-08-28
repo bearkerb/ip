73548f4f14f6e9495cb1ee296bada98d1de9ef1b
bearkerb <gabrieltan7879@gmail.com>
2025-08-28 14:51:33 +0800

    Add date time string parsing for event and deadline

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,22 +1,48 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    private String start;
-    private String end;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private LocalTime startTime;
+    private LocalTime endTime;
 
     public Event(String name, String start, String end) {
         super(name);
-        this.start = start;
-        this.end = end;
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HHmm");
+        try {
+            String[] startDateTime = Parser.parseDateTimeString(start);
+            String[] endDateTime = Parser.parseDateTimeString(end);
+            this.startDate = LocalDate.parse(startDateTime[0]);
+            this.endDate = LocalDate.parse(endDateTime[0]);
+            if (startDateTime[1] != null) {
+                this.startTime = LocalTime.parse(startDateTime[1], timeFormatter);
+            }
+            if (endDateTime[1] != null) {
+                this.endTime = LocalTime.parse(endDateTime[1], timeFormatter);
+            }
 
+        } catch (DateTimeParseException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + this.start + " to: " + this.end + ")";
+        return "[E]" + super.toString() + " (from: " + dateTimeToString(startDate, startTime) + " to: "
+                + dateTimeToString(endDate, endTime) + ")";
     }
 
     @Override
     public String toDataString() {
-        return "E | " + (this.isComplete() ? "DONE" : "NOT DONE") + " | " + this.getName()
-                + " | " + this.start + " | " + this.end + "\n";
+        return "E | " + (this.isComplete() ? "DONE" : "NOT DONE") + " | " + this.getName() + " | " + this.startDate
+                + (this.startTime == null ? "" : "-" + this.startTime.format(DateTimeFormatter.ofPattern("HHmm")))
+                + " | " + this.endDate + (this.endTime == null ? "" : "-"
+                + this.endTime.format(DateTimeFormatter.ofPattern("HHmm"))) + "\n";
+    }
+
+    public String dateTimeToString(LocalDate date, LocalTime time) {
+        return date.getMonth() + " " + date.getDayOfMonth() + " " + date.getYear() + (time == null ? "" : " " + time);
     }
 }
